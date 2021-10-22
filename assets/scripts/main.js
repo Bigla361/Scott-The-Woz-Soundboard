59dfec4dbada8e77303dd33ba5979c209e33e4d6
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-10-22 17:29:48 +0100

    Added all the audio files so far in, increased the button size and updated Bagel.js and the GUI plugin.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 /*
 TODO
-Crop hey all to 1:1
-Add preloading to Bagel.js, fix issue where the video doesn't load in time
+Add part preloading assets to Bagel.js, fix issue where the video doesn't load in time
+MP3 versions
 
 Sounds to include (be careful about duplicates) (exclude some that don't work well)
 Toad moment
@@ -15,10 +15,137 @@ https://www.youtube.com/watch?v=yD2pdiovGLk
 
 
 const game = (_ => {
+    const capitalizeFirstCharacter = word => word[0].toUpperCase() + word.slice(1, word.length);
+    const sounds = [
+        {
+            src: "photoChannelDrink"
+        },
+        {
+            src: "dinnerServed"
+        },
+        {
+            src: "badMeals"
+        },
+        {
+            src: "notDoingIt"
+        },
+        {
+            src: "buyHouseKey"
+        },
+        {
+            src: "soup"
+        },
+        {
+            src: "thatsMe"
+        },
+        {
+            src: "metricPumpkin"
+        },
+        {
+            src: "triangleScamo"
+        },
+        {
+            src: "homelessApplication"
+        },
+        {
+            src: "wordReference"
+        },
+        {
+            src: "crazyRoom"
+        },
+        {
+            src: "tableSeason"
+        },
+        {
+            src: "vCard",
+            name: "V-Card"
+        },
+        {
+            src: "pinballMachine"
+        },
+        {
+            src: "sitAtTable"
+        },
+        {
+            src: "barrelBlast"
+        },
+        {
+            src: "conversationPiece"
+        },
+        {
+            src: "associateWithDead"
+        },
+        {
+            src: "wbamo"
+        },
+        {
+            src: "whiteGuy"
+        },
+        {
+            src: "donkeyKong"
+        }
+    ];
+    for (let i in sounds) {
+        let sound = sounds[i];
+        let src = sound.src;
+        sound.asset = "Sound." + capitalizeFirstCharacter(src);
+
+        if (! sound.name) {
+            let spaceName = "";
+            for (let c in src) {
+                let char = src[c];
+                let lower = char.toLowerCase();
+                if (char == lower) {
+                    spaceName += char;
+                }
+                else {
+                    spaceName += " " + lower;
+                }
+            }
+
+            sound.name = spaceName.split(" ").map(value => capitalizeFirstCharacter(value)).join(" ");
+        }
+    }
+
+    sounds.sort((a, b) => a.name.localeCompare(b.name));
+    const buttons = [];
+    let count = 0;
+    let x = 75;
+    let y = 150;
+    for (i in sounds) {
+        let sound = sounds[i];
+        buttons.push({
+            type: "button",
+            color: "red",
+            onClick: element => {
+                game.playSound(element.vars.sound);
+            },
+            onHover: sound.name,
+            size: 75,
+            x: x,
+            y: y,
+            vars: {
+                sound: sound.asset
+            }
+        });
+        x += 100;
+        count++;
+        if (count == 4) {
+            x = 75;
+            y += 100;
+            count = 0;
+        }
+    }
+
+    const soundAssets = sounds.map(sound => ({
+        id: sound.asset,
+        src: "assets/snds/mp3/" + sound.src + ".mp3",
+        ogg: "assets/snds/ogg/" + sound.src + ".ogg"
+    }));
+
     return Bagel.init({
         id: "soundboard",
         state: "intro",
-        vars: {},
         game: {
             plugins: [
                 {
@@ -37,7 +164,8 @@ const game = (_ => {
                 ],
                 imgs: [
 
-                ]
+                ],
+                snds: soundAssets
             },
             sprites: [
                 {
@@ -82,6 +210,56 @@ const game = (_ => {
                         ]
                     }
                 },
+                {
+                    id: "IntroText1",
+                    type: "text",
+                    color: "#EFEFEF",
+                    y: 550,
+                    scripts: {
+                        init: [
+                            {
+                                code: me => {
+                                    me.vars.parent = game.get.sprite("HeyAll");
+                                },
+                                stateToRun: "intro"
+                            }
+                        ],
+                        main: [
+                            {
+                                code: me => {
+                                    me.alpha = me.vars.parent.alpha;
+                                },
+                                stateToRun: "intro"
+                            }
+                        ]
+                    },
+                    text: "Scott The Woz Soundboard (unofficial)"
+                },
+                {
+                    id: "IntroText2",
+                    type: "text",
+                    color: "#EFEFEF",
+                    y: 600,
+                    scripts: {
+                        init: [
+                            {
+                                code: me => {
+                                    me.vars.parent = game.get.sprite("HeyAll");
+                                },
+                                stateToRun: "intro"
+                            }
+                        ],
+                        main: [
+                            {
+                                code: me => {
+                                    me.alpha = me.vars.parent.alpha;
+                                },
+                                stateToRun: "intro"
+                            }
+                        ]
+                    },
+                    text: "By Hedgehog125"
+                },
 
                 {
                     id: "Menu",
@@ -89,13 +267,10 @@ const game = (_ => {
                     submenu: "main",
                     submenus: {
                         main: {
-                            elements: [
-                                {
-                                    type: "button",
-                                    color: "red",
-                                    size: 50
-                                }
-                            ]
+                            elements: buttons,
+                            hoverText: {
+                                color: "#EFEFEF"
+                            }
                         }
                     },
                     stateToActivate: "menu"
@@ -109,6 +284,9 @@ const game = (_ => {
                 backgroundColor: "#202020",
                 renderer: "canvas",
                 antialiasing: true
+            },
+            loading: {
+                mode: "preload" // TODO
             }
         }
     });
